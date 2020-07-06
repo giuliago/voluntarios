050d68fda19b2e97e2e7583a2483d2f224d50b27
giuliago <giu_goliveira@hotmail.com>
2020-07-06 10:16:19 -0300

    Adicionando aba organização

--- app/lib/UI/organizationTab.dart
+++ app/lib/UI/organizationTab.dart
@@ -33,68 +33,127 @@ class _OrganizationTab extends State<OrganizationTab>
     );
   }
 
-  Widget _description(BuildContext context) {
-    double deviceWidth = MediaQuery.of(context).size.width;
-    return Container(
-        width: 320,
-        height: 180,
-        child: Column(
-          children: <Widget>[
-            Padding(
-                padding: EdgeInsets.fromLTRB(0, 20, 220, 0),
-                child: Text(
-                  'Descrição',
-                  style: TextStyle(fontSize: 20, color: Colors.lightGreen[700]),
-                )),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Text(
-                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, color: Colors.black),
-              ),
-            ),
-            GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => Home()));
-                },
-                child: Text('Participantes (200)...',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(fontSize: 16, color: Colors.lightBlue))),
-            Padding(
-                padding: EdgeInsets.fromLTRB(20, 10, 0, 10),
-                child: Expanded(
-                    child: Row(children: <Widget>[
-                  SizedBox(
-                      width: deviceWidth - 20,
-                      height: 40,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: 7,
-                          itemBuilder: (BuildContext context, int index) {
-                            return _buildAvatar(context, 400, 40);
-                          }))
-                ]))),
-          ],
-        ));
-  }
-
-  Widget _buildAvatar(BuildContext context, double width, double height) {
-    return ClipOval(
-      child: new SizedBox(
-        width: width,
-        height: height,
-        child: Image(
-          image: NetworkImage('https://placeimg.com/640/480/any'),
-          fit: BoxFit.fill,
+  Widget _buildOrganizations(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          backgroundColor: Colors.cyan,
+          bottom: PreferredSize(
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 280, 20),
+                  child: Icon(
+                    Icons.business,
+                    color: Colors.white70,
+                    size: 50.0,
+                  )),
+              preferredSize: Size.fromHeight(40.0)),
+          pinned: false,
+          expandedHeight: 180.0,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text('Veja as organizações que você participa',
+                style: TextStyle(color: Colors.white)),
+          ),
         ),
-      ),
+        SliverList(
+            delegate: SliverChildListDelegate(
+          [
+            Container(
+                width: 400,
+                height: 350,
+                child: _buildYourOrganizations(context)),
+          ],
+        )),
+      ],
     );
   }
 
+  Widget _buildYourOrganizations(BuildContext context) {
+    return Expanded(
+        child: SizedBox(
+      width: 300,
+      height: 100,
+      child: ListView.builder(
+          padding: EdgeInsets.all(8),
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage:
+                            NetworkImage('https://placeimg.com/640/480/any')),
+                    title: Text('nome'),
+                    subtitle: Text('Descrição aqui'),
+                  )
+                ],
+              ),
+            );
+          }),
+    ));
+  }
+
+  Widget _description(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+            padding: EdgeInsets.fromLTRB(0, 20, 220, 0),
+            child: Text(
+              'Descrição',
+              style: TextStyle(fontSize: 20, color: Colors.lightGreen[700]),
+            )),
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: Text(
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16, color: Colors.black),
+          ),
+        ),
+        GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Home()));
+            },
+            child: Text('Participantes (200)...',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 16, color: Colors.lightBlue))),
+        Padding(
+            padding: EdgeInsets.fromLTRB(30, 10, 0, 0),
+            child: Expanded(
+                child: Row(children: <Widget>[
+              SizedBox(
+                  width: 330,
+                  height: 40,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 7,
+                      itemBuilder: (BuildContext context, int index) {
+                        return _buildAvatar(context, 40, 40);
+                      }))
+            ]))),
+      ],
+    );
+  }
+
+  Widget _buildAvatar(BuildContext context, double width, double height) {
+    return Padding(
+        padding: EdgeInsets.only(right: 5),
+        child: ClipOval(
+          child: new SizedBox(
+            width: width,
+            height: height,
+            child: Image(
+              image: NetworkImage('https://placeimg.com/640/480/any'),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ));
+  }
+
   Widget _buildAvatarSets(BuildContext context) {
-    return Stack(children: <Widget>[
+    return Column(children: <Widget>[
       Container(
         width: 400,
         height: 220,
@@ -110,6 +169,9 @@ class _OrganizationTab extends State<OrganizationTab>
             children: <Widget>[
               Padding(
                   padding: EdgeInsets.only(top: 10),
+                  child: _buildAvatar(context, 120, 120)),
+              Padding(
+                  padding: EdgeInsets.only(top: 20),
                   child: Text(
                     'Giulianna Gonçalves',
                     style: TextStyle(
@@ -138,10 +200,6 @@ class _OrganizationTab extends State<OrganizationTab>
   void dispose() {
     _controller.dispose();
     super.dispose();
-  }
-
-  Widget _buildOrganizations(BuildContext context) {
-    return Text('teste 2');
   }
 
   @override
@@ -179,7 +237,7 @@ class _OrganizationTab extends State<OrganizationTab>
             Padding(
                 padding: EdgeInsets.only(right: 20),
                 child: Icon(
-                  Icons.message,
+                  Icons.edit,
                   color: Colors.white70,
                 ))
           ],
